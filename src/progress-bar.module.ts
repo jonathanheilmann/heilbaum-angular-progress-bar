7f00ec6c1abef0a90b75fe25d6bf21c901c5197a
carstenbaumhoegger <carstenbaumhoegger@gmail.com>
2017-05-14 21:16:59 +0200

    Fix conflict marker

--- src/progress-bar.module.ts
+++ src/progress-bar.module.ts
@@ -1,8 +1,4 @@
 import { NgModule } from '@angular/core';
-<<<<<<< Updated upstream
-import { IonicModule } from 'ionic-angular';
-=======
->>>>>>> Stashed changes
 
 import { HeilbaumProgressBarComponent } from './components/heilbaum-progress-bar.component';
 
